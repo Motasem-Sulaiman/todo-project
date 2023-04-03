4a66215ccc0ae57297dc70a8d48f1a78756b0234
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-04-03 00:36:38 +0000

    add js file

---
+++ app.js
@@ -0,0 +1,29 @@
+let userName=prompt("Please enter your username");
+console.log(userName)
+let gender=prompt("Please enter your gender (male or female)");
+if(gender=="male"||gender=="female"){
+    console.log(gender)
+}
+
+let age=prompt("Please enter your age");
+if(age<=0){
+
+    alert("the age must be bigger than 0");
+}
+else(console.log(age))
+
+let userConf=confirm("press cancel to skip the welcoming message?")
+
+if(userConf==true){
+
+if(gender=="male"){
+
+    alert("Welcome Mr "+userName);
+}else if(gender=="female"){
+
+    alert ("Welcome Ms "+userName);
+}
+if(gender!=="male"&&gender!=="female"){
+alert("Please enter (male) or (female)");
+
+}}
