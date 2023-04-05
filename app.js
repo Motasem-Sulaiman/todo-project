80058357e68de0c2b29f439248e427bab140a696
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-04-05 06:03:18 +0000

    modify js

--- app.js
+++ app.js
@@ -27,3 +27,28 @@ if(gender=="male"){
 alert("Welcome "+ userName);
 
 }}
+
+let answerQues1=prompt("is this the first time u visit this website? (Yes/No)");
+
+let answerQues2=prompt("are you enjoying this website? (Yes/No)")
+
+let answerQues3=prompt("will u visit this website again? (Yes/No)")
+
+if(answerQues1===""){answerQues1="invalid"};
+
+if(answerQues2===""){answerQues2="invalid"};
+
+if(answerQues3===""){answerQues3="invalid"};
+
+
+
+let myArr=[answerQues1,answerQues2,answerQues3];
+
+function printArr(arr){
+
+    return arr;
+
+   
+}
+
+console.log(myArr);
