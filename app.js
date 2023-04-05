8190983912b91b27ea35a4fe2233a562e7264efd
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-04-05 23:15:08 +0000

    modify js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strickt'
 let userName=prompt("Please enter your username");
 console.log(userName)
 let gender=prompt("Please enter your gender (male or female)");
@@ -34,21 +35,31 @@ let answerQues2=prompt("are you enjoying this website? (Yes/No)")
 
 let answerQues3=prompt("will u visit this website again? (Yes/No)")
 
-if(answerQues1===""){answerQues1="invalid"};
-
-if(answerQues2===""){answerQues2="invalid"};
-
-if(answerQues3===""){answerQues3="invalid"};
-
-
 
 let myArr=[answerQues1,answerQues2,answerQues3];
 
-function printArr(arr){
 
-    return arr;
 
-   
-}
+function answerQues(arr){
 
-console.log(myArr);
+
+for(let i=0;i<=arr.length;i++)
+{
+    if(arr[i]===""){
+        
+        arr[i]="invalid"
+    
+    }
+        
+    }
+  
+       return arr;
+    }
+     
+    
+
+
+
+console.log(answerQues(myArr));
+
+
